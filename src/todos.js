eefae4da03b46bb62e9c756cacd3c11b775983e7
Vitalii <3373906+Rizado@users.noreply.github.com>
2025-03-12 09:49:47 +0300

    Update todos.js
    Removed unused data

--- src/todos.js
+++ src/todos.js
@@ -1,32 +1,14 @@
-const date0 = new Date(2025, 1, 1, 0, 1);
-const date1 = new Date(2025, 9, 14, 3, 0);
-const date2 = new Date(2025, 9, 15, 12, 0);
+const date1 = new Date(2025, 7, 1, 0, 0);
 
 const todos = [
-/*    {
-        title: "Встретить 2025-й год",
-        desc: "И подумать о предстоящей грустной дате в этом году",
-        image: "",
-        done: true,
-        createdAt: date0.toLocaleString(),
-        key: date0.getTime()
-    },
     {
-        title: "Отметить 50 лет",
-        desc: "И послать всех к куям в Перу :)",
+        title: "Тестовая задача",
+        desc: "добавлена по умолчанию для демонстрации работы приложения",
         image: "",
         done: false,
         createdAt: date1.toLocaleString(),
         key: date1.getTime()
-    },
-    {
-        title: "Уехать в Аргентину",
-        desc: "И быть довольным жизнью",
-        image: "",
-        done: false,
-        createdAt: date2.toLocaleString(),
-        key: date2.getTime()
-    }*/
+    }
 ];
 
 export default todos;
